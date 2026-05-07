eb53d4fba55670195c47d8f2bbb55be0d30514fd
Eduardo Oliveira <e.oliveira16@unifesp.br>
2026-05-07 15:10:11 -0300

    Add C program for Caesar cipher on scrambled words
    Implements the Caesar cipher on scrambled words, reversing the first half and decrementing the second half of characters.

---
+++ 1204.C
@@ -0,0 +1,48 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Eduardo Ribeiro de Oliveira
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1204
+Data        : 07/05/2026
+Objetivo    : Realizar a Cifra de Cesar nas palavras embaralhadas.
+Aprendizado : A percorrer o alfabeto na tabela ascii e desembaralhar palavras.
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <string.h>
+#define C 1000
+
+int main(){
+    int N, i, tamanho, metade;
+    char str[C];
+
+    scanf("%d", &N);
+    getchar();
+
+    while(N--){
+        fgets(str, sizeof(str), stdin);
+        str[strcspn(str, "\n")] = '\0';
+        
+        for(i = 0; str[i] != '\0'; i++){
+            if(str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z'){
+                str[i] += 3;
+            }
+        }
+
+        tamanho = strlen(str);
+        metade = tamanho/2;
+        char aux;
+
+        for(i = 0; i < metade; i++){
+            aux = str[i];
+            str[i] = str[tamanho - 1 - i];
+            str[tamanho - 1 - i] = aux;
+        }
+
+        for(i = metade; str[i] != '\0'; i++){
+            str[i] -= 1;
+        }
+
+        printf("%s\n", str);
+    }
+}
