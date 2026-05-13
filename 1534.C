c39864947ecd32dec5ac77a9cfa8290ce325b4e6
Eduardo Oliveira <e.oliveira16@unifesp.br>
2026-05-13 10:47:11 -0300

    Implement matrix printing with diagonal values
    This program reads an integer N and prints an NxN matrix with specific values based on the position of the elements.

---
+++ 1534.C
@@ -0,0 +1,40 @@
+/* --------------------------------------------------------------------------
+Nome        : Eduardo Ribeiro de Oliveira
+RA          : 193349
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/1534
+Exercício   : 1534
+Data        : 13/05/2026
+Objetivo    : imprimir uma matriz com o tamanho que o usuário pedir.
+Aprendizado : A impressão de uma matriz NxN que tenha o deslocamento diagonal do valores.
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int main(){
+    int N, i, j;
+
+    while(scanf("%d", &N) != EOF){
+        int matriz[N][N];
+
+        for(i = 0; i < N; i++){
+            for(j = 0; j < N; j++){
+                if(i + j == N - 1){
+                    matriz[i][j] = 2;
+                } 
+                else if(i == j){
+                    matriz[i][j] = 1;
+                }
+                else{
+                    matriz[i][j] = 3;
+                }
+
+                printf("%d", matriz[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
